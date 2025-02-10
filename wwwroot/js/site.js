76782c48dc6b383f1692793cf142dc3a0ed091cd
Дмитрий <lyahov@basasport.ru>
2025-02-10 10:46:35 +0300

    Fix bugs with SignalR

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -16,33 +16,3 @@ function toggleConfirmPasswordVisibility() {
     passwordIcon.classList.toggle('bi-eye-slash', isPasswordVisible);
 }
 
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/ordersHub")
-    .build();
-
-connection.on("ReceiveNewOrder", function(order) {
-    const pickupDate = new Date(order.pickupDate).toLocaleDateString();
-    const orderHtml = `
-            <div class="col-12 col-md-6 col-lg-4 mb-4" id="order-${order.orderId}">
-                <div class="card animate__animated animate__fadeInUp">
-                    <div class="card-header">
-                        Заказ № ${order.orderId}
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            <strong>Отправитель:</strong> ${order.senderCity}, ${order.senderAddress}<br />
-                            <strong>Получатель:</strong> ${order.recipientCity}, ${order.recipientAddress}<br />
-                            <strong>Вес груза:</strong> ${order.weight} кг<br />
-                            <strong>Дата забора:</strong> ${pickupDate}
-                        </p>
-                        <a href="/Orders/Details/${order.orderId}" class="btn btn-primary">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-        `;
-    document.getElementById("ordersRow").insertAdjacentHTML("afterbegin", orderHtml);
-});
-
-connection.start().catch(function (err) {
-    return console.error(err.toString());
-});
