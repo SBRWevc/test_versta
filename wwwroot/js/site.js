0c3ff00d4750ca64f086833f866a0252f15b159a
Дмитрий <lyahov@basasport.ru>
2025-02-10 10:18:18 +0300

    Update password toggle

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('passwordInput');
+    const passwordIcon = document.getElementById('passwordIcon');
+    const isPasswordVisible = passwordInput.type === 'text';
+    passwordInput.type = isPasswordVisible ? 'password' : 'text';
+    passwordIcon.classList.toggle('bi-eye', !isPasswordVisible);
+    passwordIcon.classList.toggle('bi-eye-slash', isPasswordVisible);
+}
+
+function toggleConfirmPasswordVisibility() {
+    const passwordInput = document.getElementById('confirmPasswordInput');
+    const passwordIcon = document.getElementById('confirmPasswordIcon');
+    const isPasswordVisible = passwordInput.type === 'text';
+    passwordInput.type = isPasswordVisible ? 'password' : 'text';
+    passwordIcon.classList.toggle('bi-eye', !isPasswordVisible);
+    passwordIcon.classList.toggle('bi-eye-slash', isPasswordVisible);
+}
